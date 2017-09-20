fc82f59ec4e58f7a3a23bd914a12c6465e858d6e
Cindy Colucy <ckcolucy@gmail.com>
2017-09-20 09:37:21 -0400

    "start lesson 7"

--- src/com/practicaljava/lesson6/Person.java
+++ src/com/practicaljava/lesson6/Person.java
@@ -2,6 +2,9 @@ package com.practicaljava.lesson6;
 
 public class Person {
 	private String name;
+	private String address;
+	
+	int INCREASE_CAP = 20;
 	
 	public Person(String name) {
 		this.name = name;
@@ -9,6 +12,21 @@ public class Person {
 	
 	public String getName() {
 		return "Person's name is " + name;
+	}
+	
+	public void changeAddress(String address) {
+		this.address = address;
+		System.out.println("New address is " + address);
+	}
+	
+	private void giveDayOff() {
+		System.out.println("Giving day off to " + name);
+	}
+	
+	public void promote(int percent) {
+		System.out.println("Prmoting a worker...");
+		giveDayOff();
+		increasePay(percent);
 	}
 
 }
